6cae89b5b0205effc9225e73e4e49fe3b92c3450
Parijat Mukherjee <parijat_mukherjee@live.com>
2026-06-10 17:18:05 +0200

    Reapply "task 4.2 fixup: remove accidentally committed sync package"
    This reverts commit 94ec59b08950d450db98bd193ae5aac51c62758d.

--- packages/sync/src/index.ts
+++
@@ -1,3 +0,0 @@
-export type { DeviceIdentity, DeviceInfo } from "./device.js";
-export { generateDeviceIdentity, resolveDevice } from "./device.js";
-export type { SyncMessage, SyncPayload } from "./sync-message.js";
